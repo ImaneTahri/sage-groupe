7fbcc04dd35086e2377bc04a46549a8e04c616ae
Christine Kweri <ckweri@my.centennialcollege.ca>
2020-10-26 01:59:42 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,5 @@
 module.exports = 
 {
-    //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
-    "Secret": 'SomeSecret'
+    //"URI": "mongodb://localhost/contact"
+    "URI": "mongodb+srv://christine:uueAlVIKiJ44sLKJ@mongodbserver.u22hw.mongodb.net/contact?retryWrites=true&w=majority"
 }
